5c70a5087f3c269fdff5fae6b1fea1a15e0d29f0
Ariadne Conill <ariadne@dereferenced.org>
2021-03-31 09:42:36 -0600

    add base models

---
+++ secfixes_tracker/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+
+from flask import Flask
+app = Flask(__name__)
+app.config.from_pyfile(os.environ.get('SECFIXES_TRACKER_CONFIG', None), silent=False)
+
+
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy(app)
+
+
+from . import models
---
+++ secfixes_tracker/models.py
@@ -0,0 +1,35 @@
+from . import app, db
+
+
+@app.cli.command('init-db', help='Initializes the database.')
+def init_db():
+    db.create_all()
+
+
+class Vulnerability(db.Model):
+    vuln_id = db.Column(db.BigInteger, primary_key=True, index=True)
+    cve_id = db.Column(db.String(80), index=True)
+    description = db.Column(db.Text)
+    cvss3_score = db.Column(db.Numeric)
+    cvss3_vector = db.Column(db.String(80))
+
+
+class Package(db.Model):
+    package_id = db.Column(db.BigInteger, primary_key=True, index=True)
+    package_name = db.Column(db.Text)
+
+
+class PackageVersion(db.Model):
+    package_version_id = db.Column(db.BigInteger, primary_key=True, index=True)
+    package_id = db.Column(db.BigInteger, db.ForeignKey('package.package_id'), nullable=False, index=True)
+    version = db.Column(db.String(80))
+    package = db.relationship('Package', backref='versions')
+
+
+class VulnerabilityState(db.Model):
+    vuln_state_id = db.Column(db.BigInteger, primary_key=True, index=True)
+    vuln_id = db.Column(db.BigInteger, db.ForeignKey('vulnerability.vuln_id'), nullable=False, index=True)
+    package_version_id = db.Column(db.BigInteger, db.ForeignKey('package_version.package_version_id'), nullable=False, index=True)
+    fixed = db.Column(db.Boolean)
+    vuln = db.relationship('Vulnerability', backref='states')
+    package_version = db.relationship('PackageVersion', backref='states')
