cb230c6a4a6c9e8cb6f84a08d9ec69d54d66e230
Ariadne Conill <ariadne@dereferenced.org>
2021-04-20 06:59:00 -0600

    add index page

--- secfixes_tracker/views.py
+++ secfixes_tracker/views.py
@@ -5,6 +5,11 @@ from . import app, db
 from .models import Vulnerability
 
 
+@app.route('/')
+def show_index():
+    return render_template('index.html')
+
+
 @app.route('/vuln/<cve_id>')
 def show_vulnerability(cve_id):
     v = Vulnerability.query.filter_by(cve_id=cve_id).first_or_404()
