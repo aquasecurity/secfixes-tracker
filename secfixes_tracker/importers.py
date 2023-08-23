35ec4941f80fcc470e794206ac43ed89a625c015
Natanael Copa <ncopa@alpinelinux.org>
2023-08-23 18:59:43 +0200

    importers: style fixes

--- secfixes_tracker/importers.py
+++ secfixes_tracker/importers.py
@@ -1,7 +1,6 @@
 import click
 import json
 import gzip
-import uuid
 import requests
 import tarfile
 import tempfile
@@ -9,17 +8,19 @@ import yaml
 
 
 from io import TextIOWrapper
-from pprint import pprint
 from . import db
 from .models import Vulnerability, Package, PackageVersion, VulnerabilityState, CPEMatch, VulnerabilityReference
 from .version import APKVersion
 
 
-rewrite_python = lambda x: 'py3-' + x.replace('_', '-').lower()
-rewrite_ruby = lambda x: 'ruby-' + x.replace('_', '-').lower()
-rewrite_perl = lambda x: 'perl-' + x.replace('_', '-').replace('::', '-').lower()
-rewrite_lua = lambda x: 'lua-' + x.replace('_', '-').lower()
-rewrite_vscode = lambda x: 'vscode-' + x.replace('_', '-').lower()
+def rewrite_python(x): return 'py3-' + x.replace('_', '-').lower()
+def rewrite_ruby(x): return 'ruby-' + x.replace('_', '-').lower()
+def rewrite_perl(x): return 'perl-' + x.replace('_',
+                                                '-').replace('::', '-').lower()
+
+
+def rewrite_lua(x): return 'lua-' + x.replace('_', '-').lower()
+def rewrite_vscode(x): return 'vscode-' + x.replace('_', '-').lower()
 
 
 LANGUAGE_REWRITERS = {
@@ -57,7 +58,6 @@ def register(app):
         db.session.commit()
         print(f'I: Imported NVD feed successfully.')
 
-
     def process_nvd_cve_reference(vuln: Vulnerability, item: dict):
         ref_type = item['refsource']
         ref_tags = item.get('tags', [])
@@ -72,10 +72,8 @@ def register(app):
         ref = VulnerabilityReference.find_or_create(vuln, ref_type, ref_uri)
         db.session.add(ref)
 
-
     def process_nvd_cve_references(vuln: Vulnerability, refs: list):
         [process_nvd_cve_reference(vuln, item) for item in refs]
-
 
     def process_nvd_cve_item(item: dict):
         if 'cve' not in item:
@@ -88,14 +86,16 @@ def register(app):
         if not cve_id:
             return
 
-        cve_description = cve.get('description', {}).get('description_data', [])
+        cve_description = cve.get('description', {}).get(
+            'description_data', [])
         if not cve_description:
             return
 
         cve_description_text = cve_description[0]['value']
         print(f'I: Processing {cve_id}.')
 
-        impact = item.get('impact', {}).get('baseMetricV3', {}).get('cvssV3', {})
+        impact = item.get('impact', {}).get(
+            'baseMetricV3', {}).get('cvssV3', {})
 
         cvss3_score = impact.get('baseScore', None)
         cvss3_vector = impact.get('vectorString', None)
@@ -111,13 +111,13 @@ def register(app):
             process_nvd_cve_configurations(vuln, item['configurations'])
 
         if 'references' in cve:
-            process_nvd_cve_references(vuln, cve['references']['reference_data'])
-
+            process_nvd_cve_references(
+                vuln, cve['references']['reference_data'])
 
     def match_uses_version_ranges(match: dict) -> bool:
-        version_match_keywords = {'versionStartIncluding', 'versionStartExcluding', 'versionEndIncluding', 'versionEndExcluding'}
+        version_match_keywords = {
+            'versionStartIncluding', 'versionStartExcluding', 'versionEndIncluding', 'versionEndExcluding'}
         return version_match_keywords & set(match.keys()) != set()
-
 
     def process_nvd_cve_configurations(vuln: Vulnerability, configuration: dict):
         global LANGUAGE_REWRITERS
@@ -164,8 +164,10 @@ def register(app):
             # so extract this data
             using_version_ranges = match_uses_version_ranges(match)
 
-            max_version = match.get('versionEndIncluding', match.get('versionEndExcluding', source_version))
-            min_version = match.get('versionStartIncluding', match.get('versionStartExcluding', None))
+            max_version = match.get('versionEndIncluding', match.get(
+                'versionEndExcluding', source_version))
+            min_version = match.get('versionStartIncluding', match.get(
+                'versionStartExcluding', None))
 
             min_version_op = '=='
             max_version_op = '=='
@@ -181,8 +183,8 @@ def register(app):
                 if 'versionEndExcluding' in match:
                     max_version_op = '<'
 
-            process_nvd_cve_configuration_item(vuln, source_pkgname, min_version, min_version_op, max_version, max_version_op, vulnerable, cpe_uri)
-
+            process_nvd_cve_configuration_item(
+                vuln, source_pkgname, min_version, min_version_op, max_version, max_version_op, vulnerable, cpe_uri)
 
     def process_nvd_cve_configuration_item(vuln: Vulnerability, source_pkgname: str,
                                            min_version: str, min_version_op: str,
@@ -190,9 +192,9 @@ def register(app):
         pkg = Package.find_or_create(source_pkgname)
         db.session.add(pkg)
 
-        cm = CPEMatch.find_or_create(pkg, vuln, min_version, min_version_op, max_version, max_version_op, vulnerable, cpe_uri)
+        cm = CPEMatch.find_or_create(
+            pkg, vuln, min_version, min_version_op, max_version, max_version_op, vulnerable, cpe_uri)
         db.session.add(cm)
-
 
     @app.cli.command('import-secfixes', help='Import secfixes feeds.')
     @click.argument('repo', required=False)
@@ -210,7 +212,6 @@ def register(app):
         for repo, uri in repositories.items():
             import_secfixes_feed(repo, uri)
 
-
     def import_secfixes_feed(repo: str, uri: str):
         print(f'I: [{repo}] Downloading {uri}')
 
@@ -222,7 +223,6 @@ def register(app):
         for package in packages:
             import_secfixes_package(repo, package['pkg'])
         db.session.commit()
-
 
     def import_secfixes_package(repo: str, package: dict):
         pkg = Package.find_or_create(package['name'])
@@ -243,7 +243,6 @@ def register(app):
 
                 db.session.add(state)
 
-
     @app.cli.command('import-rejections', help='Import security rejections feeds.')
     @click.argument('repo', required=False)
     def import_security_rejections(repo: str):
@@ -260,7 +259,6 @@ def register(app):
         for repo, uri in app.config.get('SECURITY_REJECTIONS', {}).items():
             import_security_rejections_feed(repo, uri)
 
-
     def import_security_rejections_feed(repo: str, uri: str):
         print(f'I: [{repo}] Downloading {uri}')
 
@@ -268,11 +266,12 @@ def register(app):
 
         try:
             feed = yaml.load(r.content, Loader=yaml.SafeLoader)
-            [import_security_rejections_package(repo, k, v) for k, v in feed.items()]
+            [import_security_rejections_package(
+                repo, k, v) for k, v in feed.items()]
         except Exception as e:
-            print(f'E: Encountered {e} while parsing security rejections feed.')
+            print(
+                f'E: Encountered {e} while parsing security rejections feed.')
         db.session.commit()
-
 
     def import_security_rejections_package(repo: str, pkgname: str, cves: list):
         pkg = Package.find_or_create(pkgname)
@@ -289,7 +288,6 @@ def register(app):
             state.fixed = True
 
             db.session.add(state)
-
 
     @app.cli.command('import-apkindex', help='Import APK repository indices.')
     @click.argument('repo', required=False)
@@ -319,7 +317,6 @@ def register(app):
 
             import_apkindex_payload(repo, f)
 
-
     def import_apkindex_pkg(pkg: dict, repo: str):
         origin = pkg.get('o', pkg['P'])
         p = Package.find_or_create(origin)
@@ -332,7 +329,6 @@ def register(app):
             pkgver.maintainer = pkg.get('m', None)
 
         db.session.add(pkgver)
-
 
     def import_apkindex_idx(index_data, repo: str):
         current_pkg = {}
@@ -347,7 +343,6 @@ def register(app):
                 current_pkg[data[0]] = data[1]
         db.session.commit()
 
-
     def import_apkindex_payload(repo: str, file):
         PackageVersion.query.filter_by(repo=repo).update(dict(published=False))
         db.session.commit()
@@ -360,14 +355,14 @@ def register(app):
                     data = tf.extractfile(tarentry)
                     import_apkindex_idx(TextIOWrapper(data), repo)
 
-
     @app.cli.command('update-states', help='Update the package vulnerability states.')
     @click.argument('repo', required=False)
     def update_states(repo: str):
         repositories = app.config.get('SECFIXES_REPOSITORIES', {})
         if repo:
             if not repositories.get(repo):
-                print(f"E: Repository {repo} not found in SECFIXES_REPOSITORIES.")
+                print(
+                    f"E: Repository {repo} not found in SECFIXES_REPOSITORIES.")
                 exit(1)
 
             update_states_for_repo_tag(repo)
@@ -376,14 +371,12 @@ def register(app):
         for repo, _ in repositories.items():
             update_states_for_repo_tag(repo)
 
-
     def update_states_for_repo_tag(repo: str):
         print(f'I: [{repo}] Processing updates.')
 
         for pkgver in PackageVersion.query.filter_by(repo=repo, published=True):
             update_states_for_pkgver(pkgver)
         db.session.commit()
-
 
     def update_states_for_pkgver(pkgver: PackageVersion):
         pkg = pkgver.package
@@ -393,7 +386,6 @@ def register(app):
         # walk the CPE matches to find associated vulnerabilities
         for cpe_match in pkg.cpe_matches:
             update_states_if_pkgver_matches_cpe_match(pkgver, cpe_match)
-
 
     def update_states_if_pkgver_matches_cpe_match(pkgver: PackageVersion, cpe_match: CPEMatch):
         vuln = cpe_match.vuln
@@ -406,16 +398,21 @@ def register(app):
             return
 
         # Look for a fixed VulnerabilityState that is older than pkgver.
-        fixed_states = VulnerabilityState.query.filter_by(vuln_id=vuln.vuln_id, fixed=True).all()
-        fixed_states = [state for state in fixed_states if state.package_version.repo == pkgver.repo]
-        fixed_states = sorted(fixed_states, key=lambda x: APKVersion(x.package_version.version))
+        fixed_states = VulnerabilityState.query.filter_by(
+            vuln_id=vuln.vuln_id, fixed=True).all()
+        fixed_states = [
+            state for state in fixed_states if state.package_version.repo == pkgver.repo]
+        fixed_states = sorted(
+            fixed_states, key=lambda x: APKVersion(x.package_version.version))
 
         fixed_state = fixed_states[0] if fixed_states else None
         fixed = False
         if not fixed_state:
-            print(f'I: No fix recorded against any {pkgver.package} version for {vuln}')
+            print(
+                f'I: No fix recorded against any {pkgver.package} version for {vuln}')
         else:
-            print(f'I: Fix recorded in {fixed_state.package_version} for {vuln}')
+            print(
+                f'I: Fix recorded in {fixed_state.package_version} for {vuln}')
 
             fv = APKVersion(fixed_state.package_version.version)
             fixed = pv >= fv
