4bd8c1e0251775846755b8537cd3e9c16ba64571
Kevin Daudt <kdaudt@alpinelinux.org>
2025-08-22 20:47:32 +0200

    secfixes-cli: equalize schema with existing database
    - Names of indexes
    - Column types
    - Column order
    - Checks on boolean fields

--- secfixes/model.go
+++ secfixes/model.go
@@ -1,25 +1,26 @@
 package secfixes
 
 type Vulnerability struct {
-	CveID       string `gorm:"index"`
-	Description string
-	Cvss3Vector string
-	CPEMatches  []CPEMatch `gorm:"foreignKey:vuln_id"`
-	VulnID      int        `gorm:"primaryKey"`
-	Cvss3Score  float64
+	CveID                  string `gorm:"type:varchar(80);index:ix_vulnerability_cve_id"`
+	Description            string
+	Cvss3Vector            string                   `gorm:"type:varchar(80)"`
+	CPEMatches             []CPEMatch               `gorm:"foreignKey:vuln_id"`
+	VulnerabilityReference []VulnerabilityReference `gorm:"foreignKey:vuln_id"`
+	VulnID                 int                      `gorm:"primaryKey;not null;index:ix_vulnerability_vuln_id"`
+	Cvss3Score             float64                  `gorm:"type:numeric"`
 }
 
 type Package struct {
-	PackageName string     `gorm:"index"`
+	PackageName string     `gorm:"index:ix_package_"`
 	CPEMatches  []CPEMatch `gorm:"foreignKey:vuln_id"`
-	PackageID   int        `gorm:"primaryKey"`
+	PackageID   int        `gorm:"primaryKey;not null;index:ix_package_package_id"`
 }
 
 type VulnerabilityReference struct {
-	RefType   string
-	RefUri    string
-	VulnRefID int `gorm:"primaryKey"`
-	VulnID    int
+	RefType   string `gorm:"type:varchar(80)"`
+	RefUri    string `gorm:"index:ix_vulnerability_reference_vuln_ref_uri"`
+	VulnRefID int    `gorm:"primaryKey;not null;index:ix_vulnerability_reference_vuln_ref_id"`
+	VulnID    int    `gorm:"not null;ix_vulnerability_reference_vuln_id"`
 }
 
 // type PackageVersion struct {
@@ -32,15 +33,15 @@ type VulnerabilityReference struct {
 // }
 
 type CPEMatch struct {
-	MinimumVersion   *string
-	MaximumVersion   *string
-	MinimumVersionOP string
-	MaximumVersionOP string
+	MinimumVersion   *string `gorm:"type:varchar(80)"`
+	MaximumVersion   *string `gorm:"type:varchar(80)"`
+	MinimumVersionOP string  `gorm:"type:varchar(5)"`
+	MaximumVersionOP string  `gorm:"type:varchar(5)"`
 	CpeUri           string
-	CpeMatchID       int `gorm:"primaryKey"`
-	VulnID           int
-	PackageID        int
-	Vulnerable       bool
+	CpeMatchID       int  `gorm:"primaryKey;not null;index:ix_cpe_match_cpe_match_id"`
+	VulnID           int  `gorm:"not null;index:ix_cpe_match_vuln_id"`
+	PackageID        int  `gorm:"not null;index:ix_cpe_match_package_id"`
+	Vulnerable       bool `gorm:"type:boolean;check:vulnerable IN (0, 1)"`
 }
 
 // type VulnerabilityState struct {
